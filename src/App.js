5379e0c5213a3b122b613772dfd3d7b5eb35528e
taehwan01 <monte6198@gmail.com>
2023-09-26 02:00:02 +0900

    feat: Header, Footer 추가

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+
+import Footer from './components/Footer/Footer';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Header />
+      <Routes>
+        <Route
+          path='/'
+          element={
+            <div style={{ flex: 1 }}>
+              <h1>CONTENT</h1>
+            </div>
+          }
+        />
+      </Routes>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,26 @@
+import styles from './Footer.module.scss';
+
+function Footer() {
+  return (
+    <div className={styles.footer}>
+      <div className={styles.icon}>
+        <h1>I</h1>
+        <span style={{ fontSize: '8px' }}>뒤로가기</span>
+      </div>
+      <div className={styles.icon}>
+        <h1>C</h1>
+        <span style={{ fontSize: '8px' }}>홈</span>
+      </div>
+      <div className={styles.icon}>
+        <h1>O</h1>
+        <span style={{ fontSize: '8px' }}>문의</span>
+      </div>
+      <div className={styles.icon}>
+        <h1>N</h1>
+        <span style={{ fontSize: '8px' }}>만든이</span>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
