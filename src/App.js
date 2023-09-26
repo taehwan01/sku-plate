3091866229dbcb7f6e7da050e097de5324dddf54
Monte <monte6198@gmail.com>
2023-09-26 14:35:17 +0900

    feat: 아이폰 도구 막대 화면 가림 해결

--- src/App.js
+++ src/App.js
@@ -2,8 +2,16 @@ import { Route, Routes } from 'react-router-dom';
 
 import Footer from './components/Footer/Footer';
 import Header from './components/Header';
+import { useEffect } from 'react';
 
 function App() {
+  let vh = 0;
+  useEffect(() => {
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  }, []);
+
   return (
     <div className='App'>
       <Header />
