5aec09643cf993a4423b5510ac177ca10e4377bb
Monte <monte6198@gmail.com>
2023-09-26 17:29:28 +0900

    design: 아이폰 웹뷰 기준 수정

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import { Route, Routes } from 'react-router-dom';
 
 import Footer from './components/Footer/Footer';
-import Header from './components/Header';
+import Header from './components/Header/Header';
 import { useEffect } from 'react';
 
 function App() {
-  let vh = 0;
   useEffect(() => {
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
+    // const setVh = () => {
+    //   document.documentElement.style.setProperty('--vh', `${window.innerHeight}px`);
+    // };
+    // window.addEventListener('resize', setVh);
+    // setVh();
+    const setVh = () => {
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    };
+    window.addEventListener('resize', setVh);
+    setVh();
   }, []);
 
   return (
