49db4328afb9cec61e6a6a3f41563ae78c75adc1
Monte <monte6198@gmail.com>
2023-09-27 22:20:42 +0900

    feat: A2HS 기능 추가

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,19 +2,49 @@ import { useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleLeft, faMobileScreenButton } from '@fortawesome/free-solid-svg-icons';
 
+import { useAddToHomescreenPrompt } from '../../utils/A2HS';
 import styles from './Header.module.css';
+import { useEffect, useState } from 'react';
 
 function Header() {
   const navigate = useNavigate();
+  const [prompt, promptToInstall] = useAddToHomescreenPrompt();
+
+  const [isAppInstalled, setIsAppInstalled] = useState(false);
+
+  const isPWAInstalled = async () => {
+    if ('getInstalledRelatedApps' in window.navigator) {
+      const relatedApps = await navigator.getInstalledRelatedApps();
+      let installed = false;
+      relatedApps.forEach((app) => {
+        //if your PWA exists in the array it is installed
+        console.log(app.platform, app.url);
+        if (app.url === 'https://yourwebsite.com/manifest.json') {
+          installed = true;
+        }
+      });
+      setIsAppInstalled(installed);
+    }
+  };
+  useEffect(() => {
+    isPWAInstalled();
+  }, []);
 
   return (
     <div className={styles.header}>
       <button className={styles.backButton} onClick={() => navigate(-1)}>
         <FontAwesomeIcon icon={faAngleLeft} />
       </button>
-      <button className={styles.backButton} onClick={() => {}}>
-        <FontAwesomeIcon icon={faMobileScreenButton} />
-      </button>
+      {!isAppInstalled ? (
+        <button className={styles.backButton} onClick={promptToInstall}>
+          <FontAwesomeIcon icon={faMobileScreenButton} />
+        </button>
+      ) : (
+        <div>
+          홈화면에 추가됐습니다. <br />
+          감사합니다.
+        </div>
+      )}
     </div>
   );
 }
