551e4df6ac3128b350e04442a06d1d8086a91bc2
Monte <monte6198@gmail.com>
2023-09-27 23:03:49 +0900

    fix: safari 홈화면 추가 버튼 비정상종료 해결

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,50 +1,64 @@
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleLeft, faMobileScreenButton } from '@fortawesome/free-solid-svg-icons';
 
-import { useAddToHomescreenPrompt } from '../../utils/A2HS';
 import styles from './Header.module.css';
-import { useEffect, useState } from 'react';
 
 function Header() {
   const navigate = useNavigate();
-  const [prompt, promptToInstall] = useAddToHomescreenPrompt();
 
-  const [isAppInstalled, setIsAppInstalled] = useState(false);
+  const [prompt, setPrompt] = useState(null);
+  const [isSafari, setIsSafari] = useState(false);
 
-  const isPWAInstalled = async () => {
-    if ('getInstalledRelatedApps' in window.navigator) {
-      const relatedApps = await navigator.getInstalledRelatedApps();
-      let installed = false;
-      relatedApps.forEach((app) => {
-        //if your PWA exists in the array it is installed
-        console.log(app.platform, app.url);
-        if (app.url === 'https://yourwebsite.com/manifest.json') {
-          installed = true;
+  useEffect(() => {
+    const handler = (event) => {
+      event.preventDefault();
+      setPrompt(event);
+    };
+
+    window.addEventListener('beforeinstallprompt', handler);
+
+    const userAgent = window.navigator.userAgent.toLowerCase();
+    if (userAgent.includes('safari') && !userAgent.includes('chrome')) {
+      setIsSafari(true);
+    }
+
+    return () => {
+      window.removeEventListener('beforeinstallprompt', handler);
+    };
+  }, []);
+
+  const handleAddToHomeScreenClick = () => {
+    if (!isSafari && prompt) {
+      prompt.prompt();
+
+      prompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('The app was added to the home screen');
+        } else {
+          console.log('The app was not added to the home screen');
         }
       });
-      setIsAppInstalled(installed);
+    } else if (isSafari) {
+      console.log('Safari에서는 홈 화면 추가 기능을 지원하지 않습니다.');
+    } else {
+      console.error('Prompt object is null');
     }
   };
-  useEffect(() => {
-    isPWAInstalled();
-  }, []);
 
   return (
     <div className={styles.header}>
       <button className={styles.backButton} onClick={() => navigate(-1)}>
         <FontAwesomeIcon icon={faAngleLeft} />
       </button>
-      {!isAppInstalled ? (
-        <button className={styles.backButton} onClick={promptToInstall}>
-          <FontAwesomeIcon icon={faMobileScreenButton} />
-        </button>
-      ) : (
-        <div>
-          홈화면에 추가됐습니다. <br />
-          감사합니다.
-        </div>
-      )}
+      <button
+        className={styles.backButton}
+        onClick={handleAddToHomeScreenClick}
+        // disabled={isSafari} // Safari 환경에서 버튼 비활성화
+      >
+        <FontAwesomeIcon icon={faMobileScreenButton} />
+      </button>
     </div>
   );
 }
