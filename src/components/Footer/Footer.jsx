8d3e1aebb8ad58f15e0e6d5cc64bb69df0d1e900
Monte <monte6198@gmail.com>
2023-09-30 01:38:51 +0900

    design: 하단바 메뉴 아이콘 추가

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,37 +1,31 @@
 import { useNavigate } from 'react-router-dom';
 
 import styles from './Footer.module.css';
+import backIcon from '../../assets/images/BACK_ICON.png';
+import homeIcon from '../../assets/images/HOME_ICON.png';
+import inquiriesIcon from '../../assets/images/INQUIRIES_ICON.png';
+import myPageIcon from '../../assets/images/MY_ICON.png';
 
 function Footer() {
   const navigate = useNavigate();
 
   return (
     <div className={styles.footer}>
-      <div
-        className={styles.icon}
-        onClick={() => {
-          navigate(-1);
-        }}
-      >
-        <h1>I</h1>
-        <span style={{ fontSize: '8px' }}>뒤로가기</span>
+      <div className={styles.icon} onClick={() => navigate(-1)}>
+        <img className={styles.menuIcons} src={backIcon} alt='뒤로가기' />
+        <span style={{ height: '14px', fontSize: '8px' }}>뒤로가기</span>
       </div>
-      <div
-        className={styles.icon}
-        onClick={() => {
-          navigate('/');
-        }}
-      >
-        <h1>C</h1>
-        <span style={{ fontSize: '8px' }}>홈</span>
+      <div className={styles.icon} onClick={() => navigate('/')}>
+        <img className={styles.menuIcons} src={homeIcon} alt='홈' />
+        <span style={{ height: '14px', fontSize: '8px' }}>홈</span>
       </div>
       <div className={styles.icon} onClick={() => navigate('/inquiries')}>
-        <h1>O</h1>
-        <span style={{ fontSize: '8px' }}>문의</span>
+        <img className={styles.menuIcons} src={inquiriesIcon} alt='문의' />
+        <span style={{ height: '14px', fontSize: '8px' }}>문의</span>
       </div>
       <div className={styles.icon} onClick={() => navigate('/my-page')}>
-        <h1>N</h1>
-        <span style={{ fontSize: '8px' }}>만든이</span>
+        <img className={styles.menuIcons} src={myPageIcon} alt='만든이' />
+        <span style={{ height: '14px', fontSize: '8px' }}>만든이</span>
       </div>
     </div>
   );
