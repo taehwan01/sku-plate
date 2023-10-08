146f006e504300245d309a58d2d5837b1e6ebd77
Monte <monte6198@gmail.com>
2023-10-08 22:49:49 +0900

    design: footer label 수정

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -13,19 +13,19 @@ function Footer() {
     <div className={styles.footer}>
       <div className={styles.icon} onClick={() => navigate(-1)}>
         <img className={styles.menuIcons} src={backIcon} alt='뒤로가기' />
-        <span style={{ height: '14px', fontSize: '8px' }}>뒤로가기</span>
+        <span className={styles.menuLabels}>뒤로가기</span>
       </div>
       <div className={styles.icon} onClick={() => navigate('/')}>
         <img className={styles.menuIcons} src={homeIcon} alt='홈' />
-        <span style={{ height: '14px', fontSize: '8px' }}>홈</span>
+        <span className={styles.menuLabels}>홈</span>
       </div>
       <div className={styles.icon} onClick={() => navigate('/inquiries')}>
         <img className={styles.menuIcons} src={inquiriesIcon} alt='문의' />
-        <span style={{ height: '14px', fontSize: '8px' }}>문의</span>
+        <span className={styles.menuLabels}>문의</span>
       </div>
       <div className={styles.icon} onClick={() => navigate('/my-page')}>
         <img className={styles.menuIcons} src={myPageIcon} alt='만든이' />
-        <span style={{ height: '14px', fontSize: '8px' }}>만든이</span>
+        <span className={styles.menuLabels}>마이페이지</span>
       </div>
     </div>
   );
