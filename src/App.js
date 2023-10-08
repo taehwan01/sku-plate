922c6f354ccd745c43dcbd82cc87474517265b1e
taehwan01 <monte6198@gmail.com>
2023-10-09 02:05:21 +0900

    feat: 카테고리 선택란 추가

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
       <Header />
       <Routes style={{ flex: 1 }}>
         <Route path='/' element={<Home />} />
-        <Route path='/categories/:category' element={<Restaurants />} />
+        <Route path='/categories/:currentCategory' element={<Restaurants />} />
         <Route path='/inquiries' element={<h1>문의하기</h1>} />
         <Route path='/my-page' element={<h1>MY 페이지</h1>} />
       </Routes>
--- src/pages/Restaurants/Restaurants.jsx
+++ src/pages/Restaurants/Restaurants.jsx
@@ -1,12 +1,43 @@
-import { useParams } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+
+import styles from './Restaurants.module.css';
+import SearchBox from '../../components/SearchBox/SearchBox';
+import { categories } from '../../data/categories';
 
 function Restaurants() {
-  const { category } = useParams();
+  const { currentCategory } = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    console.log(currentCategory);
+  }, [currentCategory]);
 
   return (
     <div>
-      <h1>Restaruant</h1>
-      <h3>{category}</h3>
+      <section aria-label='search restaurant'>
+        <SearchBox />
+      </section>
+      <hr />
+      <section className={styles.categories}>
+        {categories.map((category) => (
+          <div
+            key={category.id}
+            className={styles.category}
+            onClick={() => navigate(`/categories/${category.name_eng}`)}
+          >
+            <span>{category.name}</span>
+            <div
+              className={styles.selectedCategoryTag}
+              style={{ backgroundColor: `${currentCategory === category.name_eng ? '#e05757' : 'transparent'}` }}
+            >
+              {/* selected category tagline */}
+            </div>
+          </div>
+        ))}
+      </section>
+      <hr />
+      <section aria-label='classified restaurants'>{/* <ClassifiedRestaurants/> */}</section>
     </div>
   );
 }
